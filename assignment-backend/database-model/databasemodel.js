065f0a45f5723a9b2af015daa2807a45471e1e8e
Harshit tomar <80025240+levi-1010-byte@users.noreply.github.com>
2023-09-18 13:27:01 +0530

    Update databasemodel.js

--- assignment-backend/database-model/databasemodel.js
+++ assignment-backend/database-model/databasemodel.js
@@ -1,13 +1,9 @@
 const { Client } = require('pg');
 require('dotenv').config();
 
-
 const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: 5432, 
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 client.connect((err) => {
@@ -15,7 +11,35 @@ client.connect((err) => {
     console.error('Error connecting to database:', err);
   } else {
     console.log('Connected to Postgres database');
+
+  
+    createEmployeeTable();
   }
 });
+
+function createEmployeeTable() {
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS employeeDatatable (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      department VARCHAR(255),
+      position VARCHAR(255),
+      start_date DATE
+    );
+  `;
+
+  client.query(createTableQuery, (err, result) => {
+    if (err) {
+      console.error('Error creating employee table:', err);
+    } else {
+      console.log('Employee table created successfully');
+    }
+
+  
+    client.end();
+  });
+}
+
 
 module.exports = client;
